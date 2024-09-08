bb40b55a8bef0b911300b15c1e109480b6bf6fd5
Alvar Maciel <alvarmaciel@gmail.com>
2024-09-08 08:25:13 -0300

    add title, author and file name dinamically

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 import os 
 
-project = 'omnivook'
+project = os.environ.get("PROJECT_NAME", "omnivook")
 copyright = '2024, Martín Gaitán'
 author = 'Martín Gaitán'
 
@@ -8,7 +8,7 @@ extensions = ["myst_parser"]
 source_suffix = {'.md': 'markdown'}
 
 epub_title = os.environ.get("EPUB_TITLE", "Omnivook")
-epub_author = 'Martin Gaitan'
+epub_author = os.environ.get("EPUB_AUTHORS", "Martín Gaitán")
 epub_show_urls = 'no'
 version = f'0.1.{os.environ.get("GITHUB_RUN_ID", 0)}'
 suppress_warnings = ["epub.unknown_project_files"]
